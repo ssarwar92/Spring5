d2c2b7dbed6a1dcb0525ab2b974010511c3750c8
Sarwar <gssarwar@gmail.com>
2018-09-03 23:40:49 +0530

    commit after @Primary

--- src/main/java/com/gs/spring5/springin5steps/QuickShortAlgorithm.java
+++ src/main/java/com/gs/spring5/springin5steps/QuickShortAlgorithm.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-//@Primary
+@Primary
 public class QuickShortAlgorithm implements  SortAlgorithms
 {
   public int[] sort(int[] numbers){
